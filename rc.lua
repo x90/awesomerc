86e65557540ca79b212a494a5cf264aa6917d5cb
reus <reusee@gmail.com>
2012-10-19 06:20:32 +0800

    no border at startup

--- rc.lua
+++ rc.lua
@@ -362,7 +362,7 @@ root.keys(globalkeys)
 awful.rules.rules = {
     -- All clients will match this rule.
     { rule = { },
-      properties = { border_width = beautiful.border_width,
+      properties = { border_width = 0,
                      border_color = beautiful.border_normal,
                      focus = true,
                      keys = clientkeys,
